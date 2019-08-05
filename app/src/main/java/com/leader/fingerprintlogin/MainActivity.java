f15366cb3736210e4087603caa307660055c9f6f
Nabil Salim <s.nabil@lesoft.com>
2019-08-05 16:18:05 +0530

    just biometric prompt used now

--- app/src/main/java/com/leader/fingerprintlogin/MainActivity.java
+++ app/src/main/java/com/leader/fingerprintlogin/MainActivity.java
@@ -39,12 +39,12 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
 public class MainActivity extends AppCompatActivity {
-    private FingerprintManager fingerprintManager;
-    private KeyguardManager keyguardManager;
-
-    private KeyStore keyStore;
-    private Cipher cipher;
-    private String KEY_NAME = "AndroidKey";
+//    private FingerprintManager fingerprintManager;
+//    private KeyguardManager keyguardManager;
+//
+//    private KeyStore keyStore;
+//    private Cipher cipher;
+//    private String KEY_NAME = "AndroidKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,29 +75,29 @@ public class MainActivity extends AppCompatActivity {
         // Check 4: Lock screen is secured with atleast 1 type of lock
         // Check 5: Atleast 1 Fingerprint is registered
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
-            keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
-
-            if (!fingerprintManager.isHardwareDetected()) {
-                tv_finger_print.setText("Fingerprint Scanner not detected in Device");
-            } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
-                tv_finger_print.setText("Permission not granted to use Fingerprint Scanner");
-            } else if (!keyguardManager.isKeyguardSecure()) {
-                tv_finger_print.setText("Add Lock to your Phone in Settings");
-            } else if (!fingerprintManager.hasEnrolledFingerprints()) {
-                tv_finger_print.setText("You should add atleast 1 Fingerprint to use this Feature");
-            } else {
-                tv_finger_print.setText("Place your Finger on Scanner to Access the App.");
-
-                generateKey();
-                if (cipherInit()) {
-                    FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
-                    FingerprintHandler fingerprintHandler = new FingerprintHandler(this);
-                    fingerprintHandler.startAuth(fingerprintManager, cryptoObject);
-                }
-            }
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+//            keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+//
+//            if (!fingerprintManager.isHardwareDetected()) {
+//                tv_finger_print.setText("Fingerprint Scanner not detected in Device");
+//            } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+//                tv_finger_print.setText("Permission not granted to use Fingerprint Scanner");
+//            } else if (!keyguardManager.isKeyguardSecure()) {
+//                tv_finger_print.setText("Add Lock to your Phone in Settings");
+//            } else if (!fingerprintManager.hasEnrolledFingerprints()) {
+//                tv_finger_print.setText("You should add atleast 1 Fingerprint to use this Feature");
+//            } else {
+//                tv_finger_print.setText("Place your Finger on Scanner to Access the App.");
+//
+//                generateKey();
+//                if (cipherInit()) {
+//                    FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+//                    FingerprintHandler fingerprintHandler = new FingerprintHandler(this);
+//                    fingerprintHandler.startAuth(fingerprintManager, cryptoObject);
+//                }
+//            }
+//        }
 
 
         //for new biometric api
@@ -159,54 +159,54 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Noooooooo", Toast.LENGTH_SHORT).show();
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
-    private void generateKey() {
-
-        try {
-
-            keyStore = KeyStore.getInstance("AndroidKeyStore");
-            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
-
-            keyStore.load(null);
-            keyGenerator.init(new
-                    KeyGenParameterSpec.Builder(KEY_NAME,
-                    KeyProperties.PURPOSE_ENCRYPT |
-                            KeyProperties.PURPOSE_DECRYPT)
-                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-                    .setUserAuthenticationRequired(true)
-                    .setEncryptionPaddings(
-                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
-                    .build());
-            keyGenerator.generateKey();
-
-        } catch (KeyStoreException | IOException | CertificateException
-                | NoSuchAlgorithmException | InvalidAlgorithmParameterException
-                | NoSuchProviderException e) {
-
-            e.printStackTrace();
-
-        }
-
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    public boolean cipherInit() {
-        try {
-            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-            throw new RuntimeException("Failed to get Cipher", e);
-        }
-
-        try {
-            keyStore.load(null);
-
-            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME, null);
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            return true;
-        } catch (KeyPermanentlyInvalidatedException e) {
-            return false;
-        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
-            throw new RuntimeException("Failed to init Cipher", e);
-        }
-    }
+//    @TargetApi(Build.VERSION_CODES.M)
+//    private void generateKey() {
+//
+//        try {
+//
+//            keyStore = KeyStore.getInstance("AndroidKeyStore");
+//            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+//
+//            keyStore.load(null);
+//            keyGenerator.init(new
+//                    KeyGenParameterSpec.Builder(KEY_NAME,
+//                    KeyProperties.PURPOSE_ENCRYPT |
+//                            KeyProperties.PURPOSE_DECRYPT)
+//                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+//                    .setUserAuthenticationRequired(true)
+//                    .setEncryptionPaddings(
+//                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+//                    .build());
+//            keyGenerator.generateKey();
+//
+//        } catch (KeyStoreException | IOException | CertificateException
+//                | NoSuchAlgorithmException | InvalidAlgorithmParameterException
+//                | NoSuchProviderException e) {
+//
+//            e.printStackTrace();
+//
+//        }
+//
+//    }
+//
+//    @TargetApi(Build.VERSION_CODES.M)
+//    public boolean cipherInit() {
+//        try {
+//            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+//        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+//            throw new RuntimeException("Failed to get Cipher", e);
+//        }
+//
+//        try {
+//            keyStore.load(null);
+//
+//            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME, null);
+//            cipher.init(Cipher.ENCRYPT_MODE, key);
+//            return true;
+//        } catch (KeyPermanentlyInvalidatedException e) {
+//            return false;
+//        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+//            throw new RuntimeException("Failed to init Cipher", e);
+//        }
+//    }
 }
